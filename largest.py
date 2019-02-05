7c28801db307666c489c7f87a3745adaa8ad1bc2
sdixit03 <40880273+sdixit03@users.noreply.github.com>
2019-02-05 12:03:47 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,8 +1,5 @@
-print("Enter first number: ")
 num1=int(input())
-print("Enter second number: ")
 num2=int(input())
-print("Enter third number: ")
 num3=int(input())
 if num1>=num2 and num1>=num3:
     print(num1)
